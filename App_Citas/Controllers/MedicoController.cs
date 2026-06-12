a1a439adeb1f3d504576e41f2bdd07c83775dda8
MSI <MSI@DESKTOP-K8L7LMB>
2026-06-11 20:23:00 -0600

    Agregar archivos de proyecto.

---
+++ App_Citas/Controllers/MedicoController.cs
@@ -0,0 +1,49 @@
+﻿using App_Citas.Models;
+using CitasApp.Interfaces;
+using CitasApp.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CitasApp.Controllers
+{
+    public class MedicoController : Controller
+    {
+        private readonly IMedicoRepository _repo;
+        public MedicoController(IMedicoRepository repo) { _repo = repo; }
+
+        public IActionResult Index() => View(_repo.ObtenerTodos());
+
+        public IActionResult Detalle(int id)
+        {
+            var medico = _repo.ObtenerPorId(id);
+            return medico == null ? NotFound() : View(medico);
+        }
+
+        public IActionResult Crear() => View(new Medico());
+
+        [HttpPost]
+        public IActionResult Crear(Medico medico)
+        {
+            _repo.Agregar(medico);
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult Editar(int id)
+        {
+            var medico = _repo.ObtenerPorId(id);
+            return medico == null ? NotFound() : View(medico);
+        }
+
+        [HttpPost]
+        public IActionResult Editar(Medico medico)
+        {
+            _repo.Actualizar(medico);
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult Eliminar(int id)
+        {
+            _repo.Eliminar(id);
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ App_Citas/Interfaces/IPacienteRepository.cs
@@ -0,0 +1,14 @@
+﻿using App_Citas.Models;
+using CitasApp.Models;
+
+namespace CitasApp.Interfaces
+{
+    public interface IPacienteRepository
+    {
+        List<Paciente> ObtenerTodos();
+        Paciente? ObtenerPorId(int id);
+        void Agregar(Paciente paciente);
+        void Actualizar(Paciente paciente);
+        void Eliminar(int id);
+    }
+}
